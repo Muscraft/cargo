9388d5ebd8d749a82f1a1c6eb2de1ee327bce490
Scott Schafer <schaferjscott@gmail.com>
2023-05-02 10:33:16 -0500

    chore: Move `cargo-script` tests to snapbox

---
+++ tests/testsuite/single_file_packages/mod.rs
@@ -0,0 +1,19 @@
+mod permit_command;
+mod requires_nightly;
+mod requires_unstable_options;
+mod script_with_deps;
+
+fn init_registry() {
+    cargo_test_support::registry::init();
+    add_registry_packages(false);
+}
+
+fn add_registry_packages(alt: bool) {
+    cargo_test_support::registry::Package::new("baz", "1.0.0")
+        .file(
+            "src/lib.rs",
+            "pub fn hello_world() { println!(\"Hello world!\"); }",
+        )
+        .alternative(alt)
+        .publish();
+}
---
+++ tests/testsuite/single_file_packages/script_with_deps/in/file.rs
@@ -0,0 +1,10 @@
+#!/usr/bin/env cargo
+
+//! ```cargo
+//! [dependencies]
+//! baz = "1.0.0"
+//! ```
+
+fn main() {
+    baz::hello_world();
+}
